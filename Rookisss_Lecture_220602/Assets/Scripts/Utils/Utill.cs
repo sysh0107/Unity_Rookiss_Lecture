b7d162ca6fb8c6aef1a0afce1ae39a94397bbd5a
Seoyeong Lee(이서영) <syjy8809@gmail.com>
2022-07-12 10:51:22 +0900

    Add UI_Base bind and get component and gameobjects

--- Rookisss_Lecture_220602/Assets/Scripts/Utils/Utill.cs
+++ Rookisss_Lecture_220602/Assets/Scripts/Utils/Utill.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Utill
 {
+    public static GameObject FindChild(GameObject go, string name = null, bool recursive = false)
+    {
+        Transform transform = FindChild<Transform>(go, name, recursive);
+        if (transform == null)
+            return null;
+
+        return transform.gameObject;
+    }
+
     public static T FindChild<T>(GameObject go, string name = null, bool recursive = false) where T : UnityEngine.Object
     {
         if (go == null) return null;
